7804cf4cdb328a574465086b92098349e4a020a3
Arief Warazuhudien <arief.wara@gmail.com>
2024-11-30 12:13:38 +0700

    draft solution

--- src/main/java/id/levalapp/grask/Grask.java
+++ src/main/java/id/levalapp/grask/Grask.java
@@ -2,24 +2,40 @@ package id.levalapp.grask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 public class Grask {
-    private final List<Runnable> tasks = new ArrayList<>();
+    private final List<List<Runnable>> taskGroups = new ArrayList<>();
 
-    // Adds tasks to be executed in parallel
+    // Adds tasks to be executed in parallel within the same proc call
     public Grask proc(Runnable... runnables) {
+        List<Runnable> taskGroup = new ArrayList<>();
         for (Runnable runnable : runnables) {
-            tasks.add(runnable);
+            taskGroup.add(runnable);
         }
+        taskGroups.add(taskGroup);
         return this;
     }
 
-    // Executes all defined tasks using virtual threads
+    // Executes all defined tasks using ForkJoinPool, ensuring proc groups run sequentially
     public void grusk() {
-        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            tasks.forEach(executor::submit);
+        for (List<Runnable> taskGroup : taskGroups) {
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+            forkJoinPool.invoke(new TaskAction(taskGroup));
+        }
+    }
+
+    private static class TaskAction extends RecursiveAction {
+        private final List<Runnable> tasks;
+
+        TaskAction(List<Runnable> tasks) {
+            this.tasks = tasks;
+        }
+
+        @Override
+        protected void compute() {
+            tasks.forEach(Runnable::run);
         }
     }
 }
